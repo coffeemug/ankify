180e18b9d528a2909a9237f583b36fc83d3fe0f5
Vyacheslav Akhmechet <coffeemug@batboy.local>
2017-09-16 04:17:59 -0700

    tag support

--- add.py
+++ add.py
@@ -5,6 +5,7 @@ import os.path
 from anki import Collection as aopen
 from prompt_toolkit import prompt
 from prompt_toolkit.validation import Validator, ValidationError
+from prompt_toolkit.contrib.completers import WordCompleter
 from prompt_toolkit.shortcuts import print_tokens
 from prompt_toolkit.styles import style_from_dict
 from pygments.token import Token
@@ -32,27 +33,29 @@ style = style_from_dict({
 })
 
 def make_toolbar(txt):
-    return (lambda cli: [(Token.Toolbar, '(e.g. ' + txt + ') or Ctrl-c')]) if txt else None
+    return (lambda cli: [(Token.Toolbar, '(e.g. ' + txt + ') or Ctrl-c/d')]) if txt else None
 
-def uinput(txt, required=False, example=None):
+def uinput(txt, required=False, example=None, complete=None):
     print_tokens([(Token.Label, txt + '\n')], style=style)
     return prompt('> ', style=style,
                   validator = Required() if required else None,
-                  get_bottom_toolbar_tokens=make_toolbar(example))
+                  get_bottom_toolbar_tokens=make_toolbar(example),
+                  completer=WordCompleter(complete) if complete else None)
 
-def input_card():
+def input_card(x):
     term = uinput('Enter term:', required=True, example='Pigouvian tax')
     pronun = re.sub(r'^\(|\)$', '', uinput('Pronunciation?', example='pig-oo-vian'))
     defin = uinput('Definition:', required=True,
                    example='A tax levied on any market activity with negative externalities')
     details = uinput('Extra details?', example='Invented by Cambridge economist Arthur Pigou')
-    return (term, pronun, defin, details)
+    tags = uinput('Tags?', example=' '.join(x.tags.all()), complete=x.tags.all())
+    return (term, pronun, defin, details, tags)
 
 def parenify(x):
     return "({})".format(x)
 
 def print_card(card):
-    (term, pronun, defin, details) = card
+    (term, pronun, defin, details, tags) = card
     print_tokens([(Token.Card, '\n*** Card ***\n')], style=style)
     print(term)
     if pronun:
@@ -60,11 +63,12 @@ def print_card(card):
     print('---\n' + defin)
     if details:
         print('---\n' + details)
+    if tags:
+        print('---\nTags: ' + tags)
     print()
 
-def save_card(card):
-    (term, pronun, defin, details) = card
-    x = aopen(db_path)
+def save_card(card, x):
+    (term, pronun, defin, details, tags) = card
     if details:
         m = x.models.byName('Basic/reversed+details')
     else:
@@ -75,33 +79,35 @@ def save_card(card):
     n['Back'] = defin
     if details:
         n['Details'] = details
+    if tags:
+        n.setTagsFromStr(tags)
     x.addNote(n)
     x.save()
-    x.close()
     print_tokens([(Token.Saved, 'Card saved!\n\n')], style=style)
 
-def maybe_save(card):
+def savep(card):
     while True:
-        savep = prompt(get_prompt_tokens=lambda _: [(Token.Label, 'Save card? (Y\\n)'),
-                                                    (Token, '> ')],
+        should = prompt(get_prompt_tokens=lambda _: [(Token.Label, 'Save card? (Y\\n)'),
+                                                     (Token, '> ')],
                        style=style)
-        if not savep or savep == 'Y' or savep == 'y':
-            save_card(card)
-            return
-        elif savep == 'N' or savep == 'n':
+        if not should or should == 'Y' or should == 'y':
+            return True
+        elif should == 'N' or should == 'n':
             print_tokens([(Token.Dropped, 'Card dropped!\n\n')], style=style)
-            return
+            return False
 
 def main():
+    x = aopen(db_path)
     while True:
         try:
-            card = input_card()
+            card = input_card(x)
             print_card(card)
-            maybe_save(card)
+            if savep(card):
+                save_card(card, x)
         except EOFError:
             print()
-            pass
         except KeyboardInterrupt:
+            x.close()
             sys.exit()
 
 if __name__ != '__main__' or len(sys.argv) > 1:
