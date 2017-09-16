5debe2fe304ff6801600e015a4edfd154048e379
Vyacheslav Akhmechet <coffeemug@batboy.local>
2017-09-16 03:59:47 -0700

    we can save now

--- add.py
+++ add.py
@@ -1,15 +1,17 @@
 
 import sys
 import re
+import os.path
 from anki import Collection as aopen
 from prompt_toolkit import prompt
 from prompt_toolkit.validation import Validator, ValidationError
 from prompt_toolkit.shortcuts import print_tokens
 from prompt_toolkit.styles import style_from_dict
 from pygments.token import Token
+from pprint import pprint
 
 # Collection directory
-x = '/Users/coffeemug/Library/Application Support/Anki2/User 1/collection.anki2'
+db_path = os.path.expanduser('~/Library/Application Support/Anki2/User 1/collection.anki2')
 
 def usage():
     print("Usage: {}\n  add cards to Anki".format(sys.argv[0]))
@@ -30,7 +32,7 @@ style = style_from_dict({
 })
 
 def make_toolbar(txt):
-    return (lambda cli: [(Token.Toolbar, '(e.g. ' + txt + ')')]) if txt else None
+    return (lambda cli: [(Token.Toolbar, '(e.g. ' + txt + ') or Ctrl-c')]) if txt else None
 
 def uinput(txt, required=False, example=None):
     print_tokens([(Token.Label, txt + '\n')], style=style)
@@ -46,16 +48,37 @@ def input_card():
     details = uinput('Extra details?', example='Invented by Cambridge economist Arthur Pigou')
     return (term, pronun, defin, details)
 
+def parenify(x):
+    return "({})".format(x)
+
 def print_card(card):
     (term, pronun, defin, details) = card
     print_tokens([(Token.Card, '\n*** Card ***\n')], style=style)
     print(term)
     if pronun:
-        print('('+pronun+')')
+        print(parenify(pronun))
     print('---\n' + defin)
     if details:
         print('---\n' + details)
     print()
+
+def save_card(card):
+    (term, pronun, defin, details) = card
+    x = aopen(db_path)
+    if details:
+        m = x.models.byName('Basic/reversed+details')
+    else:
+        m = x.models.byName('Basic (and reversed card)')
+    x.decks.current()['mid'] = m['id']
+    n = x.newNote()
+    n['Front'] = term + ('<div>{}</div>'.format(parenify(pronun)) if pronun else '')
+    n['Back'] = defin
+    if details:
+        n['Details'] = details
+    x.addNote(n)
+    x.save()
+    x.close()
+    print_tokens([(Token.Saved, 'Card saved!\n\n')], style=style)
 
 def maybe_save(card):
     while True:
@@ -63,7 +86,7 @@ def maybe_save(card):
                                                     (Token, '> ')],
                        style=style)
         if not savep or savep == 'Y' or savep == 'y':
-            print_tokens([(Token.Saved, 'Card saved!\n\n')], style=style)
+            save_card(card)
             return
         elif savep == 'N' or savep == 'n':
             print_tokens([(Token.Dropped, 'Card dropped!\n\n')], style=style)
